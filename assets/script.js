fa1f83500a5526eb4ee13b4ed4ed76016754e9c2
CRomao <ciceroromao1923@gmail.com>
2020-01-30 20:56:13 -0300

    zerar valores / localStorage

--- assets/script.js
+++ assets/script.js
@@ -7,11 +7,30 @@ tipoPao[4] = document.querySelector("#leit");
 
 totalPaes = document.querySelector(".totalPaes");
 
+totalPaes.innerHTML = localStorage.getItem("totalPaes");
+
+for(let i=0; i<tipoPao.length; i++){
+    tipoPao[parseInt(i)].innerHTML = localStorage.getItem(String(i));
+}
+
+function cleanStorage(){
+    localStorage.setItem("totalPaes", 0);
+    totalPaes.innerHTML = 0;
+    for(let i=0; i<tipoPao.length; i++){
+        tipoPao[i].innerHTML = 0;
+        localStorage.setItem(String(i), 0);
+        parseInt(i);
+    }
+}
+
 function add(num){
     for(let i=0; i<tipoPao.length; i++){
         if(num == i){
             parseInt(tipoPao[i].innerHTML++);
+            localStorage.setItem(String(i), tipoPao[parseInt(i)].innerHTML);
+            parseInt(i);
             parseInt(totalPaes.innerHTML++);
+            localStorage.setItem("totalPaes", totalPaes.innerHTML);
             break;
         }
     }
@@ -22,7 +41,11 @@ function sub(num){
         if(num == i){
             parseInt(tipoPao[i].innerHTML--);
             parseInt(tipoPao[i].innerHTML) < 0 ? tipoPao[i].innerHTML = 0 : parseInt(totalPaes.innerHTML--);
+            localStorage.setItem(String(i), tipoPao[parseInt(i)].innerHTML);
+            parseInt(i);
+            localStorage.setItem("totalPaes", totalPaes.innerHTML);
             break;
         }
     }
 }
+
